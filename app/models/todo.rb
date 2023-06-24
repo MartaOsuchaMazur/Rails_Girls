5c6c7de3e53c37840e9ac1b505d16aacf50e6c99
MartaOsuchaMazur <93406305+MartaOsuchaMazur@users.noreply.github.com>
2023-06-24 16:05:25 +0000

    added validation

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,9 @@
 class Todo < ApplicationRecord
+    
+    validates :priority, :inclusion => 1..5
+    validates :task, :priority, :username, presence: true
+    validates_length_of :username, :minimum => 3
+
     def color
         if done?
             "table-success"
